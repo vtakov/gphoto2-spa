c58bbe1497a6c3a5ddd3b8bae68709b23efb58a8
Vladimir Takov <vladimir.takov@gmail.com>
2015-12-15 19:32:40 +0200

    Updated

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 var childProcess = require('child_process').exec;
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 function exec(command, callback) {
     return childProcess(command, execute);
